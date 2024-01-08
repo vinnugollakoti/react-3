f8abe5f8d6c0cf494aed5bc1875c0ca6ccde624c
vinay reddy gollakoti <145451307+vinnugollakoti@users.noreply.github.com>
2024-01-08 09:50:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const Container = () =>{
+    return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+       React.createElement(`div`,null,`Let's rock and roll`)
+       );
+}
+// Render the Container
+const container = document.getElementById('react-container');
+ReactDOM.render(React.createElement(Container),container);
